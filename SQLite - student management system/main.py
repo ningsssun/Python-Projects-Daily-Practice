c93b677e531456c6cbb10d30f8327942d18d6d80
sunning <ning.sssun@gmail.com>
2024-07-15 15:46:22 +0400

    load data feature

--- SQLite - student management system/main.py
+++ SQLite - student management system/main.py
@@ -1,7 +1,8 @@
 from PyQt6.QtWidgets import QApplication, QLabel, QWidget, \
-    QGridLayout, QLineEdit, QPushButton, QMainWindow, QTableWidget
+    QGridLayout, QLineEdit, QPushButton, QMainWindow, QTableWidget, QTableWidgetItem
 from PyQt6.QtGui import QAction
 import sys
+import sqlite3
 
 
 class MainWindow(QMainWindow):
@@ -23,13 +24,22 @@ class MainWindow(QMainWindow):
         self.table = QTableWidget()
         self.table.setColumnCount(4)
         self.table.setHorizontalHeaderLabels(("Id", "Name", "Course", "Mobile"))
+        self.table.verticalHeader().setVisible(False)
         self.setCentralWidget(self.table)
 
     def load_date(self):
-        self.table
+        connection = sqlite3.connect("database.db")
+        result = connection.execute("SELECT * FROM students")
+        self.table.setRowCount(0)
+        for row_number, row_data in enumerate(result):
+            self.table.insertRow(row_number)
+            for column_number, data in enumerate(row_data):
+                self.table.setItem(row_number, column_number, QTableWidgetItem(str(data)))
+        connection.close()
 
 
 app = QApplication(sys.argv)
 age_calculator = MainWindow()
 age_calculator.show()
+age_calculator.load_date()
 sys.exit(app.exec())
