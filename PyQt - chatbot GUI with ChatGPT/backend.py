fb2d7dd235fe2fa8cc8ff8260641fba1d14d6ee9
sunning <ning.sssun@gmail.com>
2024-07-16 17:40:46 +0400

    openai api

--- PyQt - chatbot GUI with ChatGPT/backend.py
+++ PyQt - chatbot GUI with ChatGPT/backend.py
@@ -1,6 +1,21 @@
-import openai
+from openai import OpenAI
 
+client = OpenAI(api_key="sk-None-13N7OzHP3u8vNC9TPRdZT3BlbkFJo27Nivzzr4YFV27OP4m5")
 
 
 class Chatbot:
-    pass
+    def __init__(self, api_key):
+        client.api_key = api_key
+    def get_response(self, user_input):
+        response = client.completions.create(
+            engine="text-davinci-003",
+            prompt=user_input,
+            max_tokens=3000,
+            temperature=0.5
+        ).choices[0].text
+        return response
+
+if __name__ == "__main__":
+    chatbot = Chatbot(client.api_key)
+
+
